7bea11a97c69fcdcd42ee8eec2b4fd4db4790326
Alexander R. Torrijos <artorrijos@gmail.com>
2017-11-19 09:38:59 +0000

    Issue #1 - Webpack configuration - updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
 const webpack = require('webpack');
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 const config = {
-    entry: './src/js/index.js',
+    entry: {
+        app: './src/js/index.js'
+    },
     output: {
-        filename: 'js/bundle.js',
+        filename: 'js/[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/html/index.html'
+        })
+    ]
 };
 
 module.exports = config;
