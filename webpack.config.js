c9376e3fdeda271de5d1acce8e1940a4e71fedb7
Alexander R Torrijos <artorrijos@gmail.com>
2017-12-31 22:43:47 +0800

    Setup scss compilation

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,31 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const config = {
     entry: {
-        app: './src/js/index.js'
+        app: './src/js/index.js',
+        base: './src/scss/main.scss'
     },
     output: {
         filename: 'js/[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    module: {
+        rules: [{
+            test: /\.(css|sass|scss)$/,
+            use: ExtractTextPlugin.extract({
+                use: ['css-loader', 'sass-loader']
+            })
+        }]
+    },
     plugins: [
         new CleanWebpackPlugin(['dist']),
+        new ExtractTextPlugin({
+            filename: "css/[name].min.css",
+            allChunks: true
+        }),
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: 'src/html/index.html'
