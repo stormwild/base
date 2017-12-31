176d7cb345475e19ca167d3571990a3d455a94c5
Alexander R. Torrijos <artorrijos@gmail.com>
2017-12-31 14:42:45 +0000

    Updated setup

--- webpack.config.js
+++ webpack.config.js
@@ -3,24 +3,17 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-<<<<<<< HEAD
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const config = {
     entry: {
+        app: './src/js/index.js',
         base: './src/scss/main.scss'
-=======
-
-const config = {
-    entry: {
-        app: './src/js/index.js'
->>>>>>> 7bea11a97c69fcdcd42ee8eec2b4fd4db4790326
     },
     output: {
         filename: 'js/[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-<<<<<<< HEAD
     module: {
         rules: [{
             test: /\.(css|sass|scss)$/,
@@ -32,15 +25,12 @@ const config = {
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new ExtractTextPlugin({
-            filename: "[name].min.css", 
+            filename: "css/[name].min.css",
             allChunks: true
-=======
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
+        }),
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: 'src/html/index.html'
->>>>>>> 7bea11a97c69fcdcd42ee8eec2b4fd4db4790326
         })
     ]
 };
